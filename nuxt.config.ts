b8637f257a805a712b5b2fbd784683c6e5fc700d
Popcone <56566419+popcone@users.noreply.github.com>
2023-07-15 15:54:53 -0700

    styles: typography, intial theme colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,12 @@ export default defineNuxtConfig({
         head: {
             title: "emmett.fyi",
             link: [
-                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300;0,400;0,500;1,400;1,500&family=Shrikhand&display=swap', },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300;0,400;0,500;1,400;1,500&display=swap', title: 'Google Font: Red Hat' },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap', title: 'Google Font: Open Sans' },
             ],
         }
     },
-    css: ['vuetify/styles'],
+    css: ['vuetify/styles', '~/assets/css/main.scss'],
     modules: [
         '@invictus.codes/nuxt-vuetify',
         '@pinia/nuxt',
@@ -31,8 +32,8 @@ export default defineNuxtConfig({
                         colors: {
                             background: '#0d0d0d',
                             surface: '#fafbfb',
-                            primary: '#337cb2',
-                            secondary: '#34bcda',
+                            primary: '#34bcda',
+                            secondary: '#337cb2',
                             tertiary: '#f7f8fa',
                             error: '#B00020',
                             info: '#2196F3',
@@ -43,9 +44,9 @@ export default defineNuxtConfig({
                     light: {
                         colors: {
                             background: '#fafbfb',
-                            surface: '#f00',
-                            primary: '#34bcda',
-                            secondary: '#337cb2',
+                            surface: '#0d0d0d',
+                            primary: '#337cb2',
+                            secondary: '#34bcda',
                             tertiary: '#f7f8fa',
                             error: '#B00020',
                             info: '#2196F3',
