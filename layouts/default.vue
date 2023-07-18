05609fb825e944b7ae9f0c19d9a31294fb6ba0fd
Popcone <56566419+popcone@users.noreply.github.com>
2023-07-18 15:42:24 -0700

    feat: add app bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,44 @@
 <script lang="ts" setup>
-import { useMainStore } from '~/store/main'
+import { useMainStore } from "~/store/main";
+import { useDisplay } from "vuetify";
 
-const mainStore = useMainStore()
+// STATE MANAGEMENT
+const mainStore = useMainStore();
 
-const theme = mainStore.theme
-const toggleTheme = mainStore.toggleTheme
+const theme = mainStore.theme;
+const toggleTheme = mainStore.toggleTheme;
 
+// DISPLAY
+const { xxl } = useDisplay();
 </script>
 
 <template>
   <v-card>
     <v-layout>
-      <v-app-bar>
+      <v-app-bar id="appbar" class="px-4" density="comfortable">
         <v-row>
-          <v-col cols="2">
-            <v-toolbar-title class="logo">Emmett</v-toolbar-title>
+          <v-col cols="6" lg="4" xxl="2">
+            <h1 class="logo text-primary">emmett.fyi</h1>
           </v-col>
-          <v-col cols="8">
-            <div class="d-flex justify-end">
-              <v-btn x-large>
-                <v-icon>mdi-github</v-icon>
+          <v-col cols="6" lg="8" class="d-flex justify-end">
+            <div>
+              <v-btn
+                href="https://github.com/popcone"
+                target="blank"
+                aria-label="Github Profile"
+                prepend-icon="mdi-github"
+                density="compact"
+                class="text-capitalize text-body-2"
+                variant="plain"
+              >
+                Github
               </v-btn>
-              <v-btn x-large>
+              <!-- <v-btn>
                 <v-icon>mdi-toggle-switch-off</v-icon>
-              </v-btn>
+              </v-btn> -->
             </div>
           </v-col>
-          <v-col cols="2">
-          </v-col>
+          <v-col v-show="xxl" cols="2"></v-col>
         </v-row>
       </v-app-bar>
 
@@ -39,7 +50,5 @@ const toggleTheme = mainStore.toggleTheme
 </template>
 
 <style scoped>
-.logo::after {
-  content: 'fyi';
-}
+/* CSS */
 </style>
