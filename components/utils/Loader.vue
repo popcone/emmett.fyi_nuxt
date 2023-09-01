49344a9efbfe567f6bb214af3c0ac3f70c9aa723
Popcone <56566419+popcone@users.noreply.github.com>
2023-09-01 11:05:02 -0700

    feat: add loader component

---
+++ components/utils/Loader.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+////
+// Props
+const props = defineProps({
+  message: { type: String, required: true },
+});
+
+const overlay = ref(true);
+
+////
+</script>
+
+<template>
+  <div id="loader" class="text-center">
+    <v-overlay
+      :model-value="overlay"
+      class="align-center justify-center bg-primary"
+      persistent
+    >
+      <v-progress-linear stream height="5"> </v-progress-linear>
+      <p>{{ message }}</p>
+    </v-overlay>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.v-overlay__scrim {
+  opacity: 1;
+}
+</style>
