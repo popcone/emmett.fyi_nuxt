4842d44d19665d8b4d37c3779bdbe9945cb6cf79
Popcone <56566419+popcone@users.noreply.github.com>
2023-09-10 19:45:00 -0700

    update: copy for homeNav store

--- store/global/homeNav.ts
+++ store/global/homeNav.ts
@@ -6,166 +6,110 @@ export const useHomeNavStore = defineStore('homeNav', {
       
     currentProject: null,
     showPortfolio: false,
-    intro: " I aim to influence & lead successful projects, improve unity among various disciplines, and maintain a flexible approach to innovate. Simply put, I like to develop some dope stuff.",
+    intro: " I aim to influence successful projects, improve unity among various disciplines, and maintain a flexible approach to innovate. Simply put, I lend my expertise to dope creations.",
     // Projects
     ///////
     projects: [
       {
         id: 1,
         title: 'teambeachbody.com',
-        tag: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.',
-        description: 'Assessments are a great way to get a baseline of your current knowledge and skills. They are also a great way to track your progress as you learn new things.',
-        source: '',
+        tag: '',
+        description: 'Interactive widgets for',
         links: {
-          internal: [
-          ],
-          external: [
-            {
-              title: 'Source: Pages',
-              url: 'https://coach.teambeachbody.com/shop/us'
-            },
-            {
-              title: 'Source: Pages',
-              url: ''
-            },
-          ],
+          source: '',
+          production: 'https://www.teambeachbody.com/shop/us',
         },
-        points: {
-          highlights: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-          stack: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-        },
+        bullets: [
+          {
+            title: 'Problem: Real-time data not accessible due to technical constraints. accessible due to technical constraints.',
+            url: '#',
+            description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
+          },
+        ],
       },
       {
         id: 1,
         title: 'Assessments',
-        tag: 'Brand-specific assessments, developed to assist users in making informed decisions about their nutrition plans. Refactored and remixed for my portfolio.',
-        description: 'Assessments are a great way to get a baseline of your current knowledge and skills. They are also a great way to track your progress as you learn new things.',
-        source: 'https://github.com/popcone/emmett.fyi_nuxt/tree/main/pages/portfolio/quiz',
+        tag: '',
+        description: 'Brand-specific assessments, developed to assist users in making informed decisions about their nutrition plans. Refactored and remixed for my portfolio.',
         links: {
-          internal: [
-            {
-              title: 'Brand #1',
-              url: '/portfolio/quiz/xbss?locale=en_us'
-            },
-            {
-              title: 'Brand #1',
-              url: '/portfolio/quiz/xbss?locale=en_us'
-            },
-          ],
-          external: [
-            {
-              title: 'Source: Pages',
-              url: 'https://github.com/popcone/emmett.fyi_nuxt/tree/main/pages/portfolio/quiz'
-            },
-            {
-              title: 'Source: Component Files',
-              url: 'https://github.com/popcone/emmett.fyi_nuxt/tree/main/components/portfolio/quiz'
-            },
-          ],
+          source: 'https://github.com/popcone/emmett.fyi_nuxt/tree/main/pages/portfolio/quiz',
+          production: '/portfolio/quiz/xbss?locale=en_us',
         },
-        points: {
-          highlights: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-          stack: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-        },
+        bullets: [
+          {
+            title: 'Problem: Real-time data not accessible due to technical constraints. accessible due to technical constraints.',
+            url: '#',
+            description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
+          },
+        ],
       },
       {
         id: 2,
         title: 'Product Guides',
-        tag: 'Assessments',
-        description: 'Product guides are a great way to get a baseline of your current knowledge and skills. They are also a great way to track your progress as you learn new things.',
-        source: '',
+        tag: '',
+        description: 'CMS managed guides',
         links: {
-          internal: [
-            {
-              title: '#',
-              url: '#'
-            },
-          ],
-          external: [
-            {
-              title: '#',
-              url: '#'
-            },
-          ],
+          source: '',
+          production: '',
+          
         },
-        points: {
-          highlights: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-          stack: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-        },
+        bullets: [
+          {
+            title: 'Problem: Real-time data not accessible due to technical constraints. accessible due to technical constraints.',
+            url: '#',
+            description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
+          },
+        ],
       },
       {
         id: 3,
-        title: 'Pricing API Sheets',
-        tag: 'Assessments',
-        description: 'Pricing API Sheets are a great way to get a baseline of your current knowledge and skills. They are also a great way to track your progress as you learn new things.',
-        source: '',
+        title: 'Product Data Sheets',
+        tag: '',
+        description: 'API driven pages for displaying product data. Single Page Applications.',
         links: {
-          internal: [
+          source: '',
+          production: 'https://tbbcoachtools.com/product-directory/us/all',
+        },
+        bullets: [
             {
-              title: '#',
-              url: '#'
+              title: 'Problem: Real-time data not accessible due to technical constraints. accessible due to technical constraints.',
+              url: '#',
+              description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
+            },
+            {
+              title: 'Solution: This approach not only provided a pragmatic solution to a complex problem',
+              url: '#',
+              description: 'To address this issue, I devised a solution involving the retrieval of weekly data sheets, which were then seamlessly integrated into a database.'
+            },
+            {
+              title: 'Process: Single Page Applications. MySQL database. CMS for Client Data Management.',
+              url: '#',
+              description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
+            },
+            {
+              title: 'Process: Single Page Applications. MySQL database. CMS for Client Data Management.',
+              url: '#',
+              description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
             },
           ],
-          external: [
-            {
-              title: '#',
-              url: '#'
-            },
-          ],
-        },
-        points: {
-          highlights: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-          stack: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-        },
       },
       {
         id: 4,
         title: 'Event Pages',
         tag: 'Assessments',
         description: 'API Event Pages are a great way to get a baseline of your current knowledge and skills. They are also a great way to track your progress as you learn new things.',
-        source: '',
         links: {
-          internal: [
-            {
-              title: '#',
-              url: '#'
-            },
-          ],
-          external: [
-            {
-              title: '#',
-              url: '#'
-            },
-          ],
+          source: '',
+          production: '',
         },
-        points: {
-          highlights: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-          stack: ["Lorem ipsum dolor sit amet, consectetur.",
-            "Sit amet, consectetur adipiscing elit.", 
-            "Sectetur adipiscing elit Lorem ipsum ."],
-        },
+        bullets: [
+          {
+            title: 'Problem: Real-time data not accessible due to technical constraints. accessible due to technical constraints.',
+            url: '#',
+            description: 'This approach not only provided a pragmatic solution to a complex problem, but also allowed for a more flexible approach to future development.'
+          },
+        ],
       },
     ],
       
@@ -193,7 +137,6 @@ interface Project {
   title: string,
   tag: string,
   description: string,
-  source: string,
-  links: { [key: string]: any },
-  points: { [key: string]: any },
+  links: { [key: string]: string }
+  bullets: Array<{ [key: string]: string }>,
 }
